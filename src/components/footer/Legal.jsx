74062688a63dfe61725319f7bcc6d6aa806c0a68
CarolBV <carolina.botella.valero@gmail.com>
2024-06-13 12:51:42 +0200

    Agregar terminos y condiciones al footer

---
+++ src/components/footer/Legal.jsx
@@ -0,0 +1,41 @@
+import Button from "../buttons/Button.jsx"
+import Icon from "../icons/Icon.jsx"
+import spainFlag from "../../assets/icons/Spain.svg"
+const terms = [
+    {
+    link: "Spain",
+    url: "#",
+  },
+  {
+    link: "Privacy",
+    url: "#",
+  },
+  {
+    link: "Google Next Commitment to Privacity",
+    url: "#",
+  },
+  {
+    link: "Sales Terms",
+    url: "#",
+  },
+  {
+    link: "Terms of Service",
+    url: "#",
+  },
+]
+const Legal = () => {
+const termList = terms.map((link) =>(
+
+    <li>
+      <Button linkButton={link} />
+    </li>
+  ));
+
+ return (
+<div>
+    <Icon name={spainFlag} describe="Spain flag" classNameProp="flag"/>
+<ul className="moreInformation">{termList}</ul> 
+</div>
+ );
+}
+export default Legal
