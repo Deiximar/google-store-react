107df560ab495cb8abb8b0416436e4a5cd452563
CarolBV <carolina.botella.valero@gmail.com>
2024-06-13 14:49:25 +0200

    Agregar descripcion de producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/nav/NavBar.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import Description from "./components/products/Description.jsx";
 function App() {
   return (
     <div>
       <NavBar />
+      <Description/>
       <Footer />
     </div>
   );
---
+++ src/components/products/Description.jsx
@@ -0,0 +1,16 @@
+const Description = () => {
+
+    return(
+
+    <div class="product-description">
+        <h1>Fitbit Inspire 3</h1>
+        <p class="subtitle">Health &amp; Fitness Tracker</p>
+        <p class="price">99.95€</p>
+        <hr/>
+      </div>
+    )
+}
+
+export default Description;
+
+
