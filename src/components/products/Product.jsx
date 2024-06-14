c70a43c7bc29d6464a1537b8056b99b3319ba94f
CarolBV <carolina.botella.valero@gmail.com>
2024-06-14 15:44:19 +0200

    Agregar componentes para imagenes principales

---
+++ src/components/products/Product.jsx
@@ -0,0 +1,18 @@
+import ProductInformation from "./ProductInformation"
+import arrow from "../../assets/icons/Arrow.svg"
+
+const Product = ()=> {
+    return (
+        <main>
+              <div class="see-product-details">
+    <img class="icon" alt="Back Icon" src={arrow}/>
+    <p>See product details</p>
+  </div>
+        <section className="product">
+      <div className="image-container"></div>
+    </section>
+    <ProductInformation/>
+    </main>
+    )
+}
+export default Product;
