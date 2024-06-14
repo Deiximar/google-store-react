ade4e7e5183138b7158dcf407b4459be0d8e5eb7
Deiximar Virriel <deiximar@gmail.com>
2024-06-14 19:51:07 +0200

    Agregar productos y modificar css

--- src/components/products/ProductInformation.jsx
+++ src/components/products/ProductInformation.jsx
@@ -3,25 +3,29 @@ import Watches from "./watches/Watches.jsx";
 import AddCart from "./AddCart.jsx";
 
 const products = [
-    {
-        title: "Fitbit Inspire 3",
-        category: "Health & Fitness Tracker",
-        price: 99.95
-    },
-    {
-        title: "Google Pixel Buds Pro",
-        category: "Music & Sound",
-        price: 229
-    },
-]
+  {
+    title: "Fitbit Inspire 3",
+    category: "Health & Fitness Tracker",
+    price: 99.95,
+  },
+  {
+    title: "Google Pixel Buds Pro",
+    category: "Music & Sound",
+    price: 229,
+  },
+];
 const ProductInformation = () => {
-    const product = products[0];
-    return (
-        <section className="productInformation">
-            <Description title={product.title} category={product.category} price={product.price} />
-            <Watches/>
-            <AddCart price={product.price}/>
-        </section>
-    )
-}
+  const product = products[0];
+  return (
+    <section>
+      <Description
+        title={product.title}
+        category={product.category}
+        price={product.price}
+      />
+      <Watches />
+      <AddCart price={product.price} />
+    </section>
+  );
+};
 export default ProductInformation;
