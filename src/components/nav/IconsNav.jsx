edde55a3d2031caf3aaf3ea66ca2ccb73c4f21b1
Deiximar Virriel <deiximar@gmail.com>
2024-06-15 13:35:36 +0200

    Crear rutas para el navbar y arreglar rutas de la importacion de images

--- src/components/nav/IconsNav.jsx
+++ src/components/nav/IconsNav.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import Icon from "../icons/Icon.jsx";
-import Search from "../../assets/icons/Search.svg";
-import Help from "../../assets/icons/Help.svg";
-import Cart from "../../assets/icons/Cart.svg";
-import Avatar from "../../assets/icons/Avatar.svg";
+import search from "../../assets/icons/Search.svg";
+import help from "../../assets/icons/Help.svg";
+import cart from "../../assets/icons/Cart.svg";
+import avatar from "../../assets/icons/Avatar.svg";
 
 const IconsNav = () => {
   return (
     <div className="nav-icons">
-      <Icon name={Search} describe="Search Icon"></Icon>
-      <Icon name={Help} describe="Help Icon"></Icon>
-      <Icon name={Cart} describe="Cart Icon"></Icon>
-      <Icon name={Avatar} describe="Avatar Icon"></Icon>
+      <Icon name={search} describe="Search Icon"></Icon>
+      <Icon name={help} describe="Help Icon"></Icon>
+      <Link to="/cart-page">
+        <Icon name={cart} describe="Cart Icon"></Icon>
+      </Link>
+      <Icon name={avatar} describe="Avatar Icon"></Icon>
     </div>
   );
 };
