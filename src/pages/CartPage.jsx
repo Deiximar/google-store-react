bea3a5fd0accdcaab24089f4f0ffd95e7501ce6b
CarolBV <carolina.botella.valero@gmail.com>
2024-06-16 17:55:43 +0200

    Agregar estructura y estilos a la seccion de carrito

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,20 @@
 import NavBar from "../components/nav/NavBar";
 import Footer from "../components/footer/Footer";
-
-const CartPage = () => {
+import ProductCart from "../components/products/cart/ProductCart.jsx"
+import Summary from "../components/products/cart/Summary.jsx";
+function CartPage() {
   return (
     <>
-      <NavBar />
-      <Footer />
+      <NavBar/>
+      <h1>CART</h1>
+      <h3>(3 items)</h3>
+      <main>
+      <ProductCart/>
+      <Summary/>
+      </main>
+      <Footer/>
+    
     </>
   );
-};
-
+}
 export default CartPage;
